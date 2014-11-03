f3fbe7869fe55886a24f8b077e857b00088dbbb9
Kalimaha <guido.barbaglia@gmail.com>
2014-11-03 15:47:52 +0100

    Test updated.

--- test/download/downloader_test.py
+++ test/download/downloader_test.py
@@ -12,8 +12,7 @@ country = '0'
 layers_to_be_downloaded = list_layers_countries_subset(product, year, day, country)
 
 # Target folder: MODIS layers will be downloaded here
-target = '/home/kalimaha/Desktop'
+target = {'target': '/home/kalimaha/Desktop/MODIS', 'product': product, 'year': year, 'day': day}
 
 # Run the downloader
-# Downloader('modis', target, layers_to_be_downloaded).download()
 Downloader('modis', target, layers_to_be_downloaded, True).download()
