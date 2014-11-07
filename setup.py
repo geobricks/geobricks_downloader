fe4a3877f77d1f3ec991ca12635a59d581437ffb
Kalimaha <guido.barbaglia@gmail.com>
2014-11-07 14:05:56 +0100

    PyPi version upgraded.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='GeobricksDownloader',
-    version='0.1.1',
+    version='0.1.2',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
