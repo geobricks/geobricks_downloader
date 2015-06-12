20a57d71c273b0b130a7431b8a112b75dfa0a061
Kalimaha <guido.barbaglia@gmail.com>
2015-06-12 13:49:01 +0200

    REST folder added.

--- test/core/test_utils.py
+++ test/core/test_utils.py
@@ -7,9 +7,16 @@ class GeobricksUtilsTest(unittest.TestCase):
     def setUp(self):
         self.a = {'name': 'Guido', 'family': 'Barbaglia', 'role': 'developer'}
         self.b = {'name': 'Simone', 'family': 'Murzilli'}
+        self.d = {'name': 'Guido', 'family': 'Barbaglia', 'role': {'title': 'developer', 'short': 'dev'}}
 
     def test_dict_merge(self):
         c = dict_merge(self.a, self.b)
         self.assertEquals(c['name'], 'Simone')
         self.assertEquals(c['family'], 'Murzilli')
         self.assertEquals(c['role'], 'developer')
+        c = dict_merge(self.a, 'Hallo, World!')
+        self.assertEquals(c, 'Hallo, World!')
+        c = dict_merge(self.d, self.b)
+        self.assertEquals(c['name'], 'Simone')
+        self.assertEquals(c['family'], 'Murzilli')
+        self.assertTrue(type(c['role']), dict)
